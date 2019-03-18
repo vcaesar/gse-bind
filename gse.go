4772d4427e0a37cce0aef8e0dce574332dd57ad6
vcaesar <vzvway@gmail.com>
2019-03-18 14:51:31 -0400

    add base code and files

---
+++ gse.go
@@ -0,0 +1,40 @@
+// Copyright 2016 The go-vgo Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// https://github.com/go-vgo/robotgo/blob/master/LICENSE
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+package main
+
+import "C"
+
+import (
+	"fmt"
+
+	"github.com/go-ego/gse"
+)
+
+func ch(str string) *C.char {
+	return C.CString(str)
+}
+
+func str(ch *C.char) string {
+	return C.GoString(ch)
+}
+
+func sf(err error) string {
+	return fmt.Sprintf("%s", err)
+}
+
+//export GetVersion
+func GetVersion() *C.char {
+	s := gse.GetVersion()
+	return ch(s)
+}
+
+
+func main() {} // Required but ignored
