ef38859f84d475e8924e3bc2f0df8c7c036be43a
vcaesar <vzvway@gmail.com>
2019-03-23 12:09:59 -0400

    add JavaScript function

---
+++ gse.js
@@ -0,0 +1,40 @@
+// var ffi = require('ffi-napi');
+var ffi = require('ffi');
+
+var lib = ffi.Library("gse", {
+    'GetVersion': ['string', []],
+    'LoadDict': ['string', ['string']],
+    'AddToken': ['void', ['string', 'long', 'string']],
+    'AddTokenForce': ['void', ['string', 'long', 'string']],
+    'Cut': ['string', ['string', 'bool']],
+    'CutAll': ['string', ['string']],
+    'CutSearch': ['string', ['string', 'bool']],
+});
+
+function getVersion() {
+    lib.GetVersion();
+}
+
+function loadDict(path) {
+    return lib.LoadDict(path);
+}
+
+function addToken(text, freq, pos) {
+    lib.AddToken(text, freq, pos);
+}
+
+function addTokenForce(text, freq, pos) {
+    lib.AddTokenForce(text, freq, pos);
+}
+
+function cut(str, hmm) {
+    return lib.Cut(str, hmm).split(" ");
+}
+
+function cutAll(str) {
+    return lib.CutAll(str).split(" ");
+}
+
+function cutSearch(str, hmm) {
+    return lib.CutSearch(str, hmm).split(" ");
+}
