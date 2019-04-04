d552b1d1915a1fb48424583ef3cd4d43dbf74dfa
vcaesar <vzvway@gmail.com>
2019-04-04 13:53:46 -0400

    add more examples

--- example/main.js
+++ example/main.js
@@ -2,4 +2,10 @@
 var gse = require('../gse');
 
 gse.loadDict();
+gse.addToken("大雨刚停", 10);
+gse.addTokenForce("今天上午", 10, "n");
+console.log(gse.find("今天上午"));
+
 console.log(gse.cut("我在大雨刚停的夜晚", true));
+console.log(gse.cutAll("我在大雨刚停的夜晚"));
+console.log(gse.cutSearch("我在大雨刚停的夜晚", true));
